3ea3a60a998ff2df0bfb4e39ce1878f675c03b47
yosra omran <yosomran@gmail.com>
2025-03-05 14:06:05 +0100

    Rooms

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,18 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import navBar from 'src/components/navBar/navBar.jsx' ;
-import headerSection from 'src/components/headerSection/headerSection.jsx';
-import cardSection from 'src/components/cardSection/cardSection.jsx';
-import footerSection from 'src/components/footerSection/footerSection.jsx';
+import ImageSlider from './components/page1/ImageSlider';
+
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <navBar/>
-    <headerSection/>
-    <cardSection/>
-    <footerSection/>
+    <ImageSlider />
   </React.StrictMode>
 );
 
